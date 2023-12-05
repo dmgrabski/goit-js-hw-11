29fe8202e0197a5de8554cd7c8e578ca04d04631
dmgrabski <dmgrabski97@gmail.com>
2023-12-05 18:08:25 +0100

    update5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
 const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
@@ -19,22 +22,27 @@ loadMoreBtn.addEventListener('click', () => {
 });
 
 async function fetchImages() {
-  const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchQuery)}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`);
-  const images = await response.json();
+  try {
+    const response = await axios.get(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(searchQuery)}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=40`);
+    const images = response.data;
 
-  if (images.hits.length === 0) {
-    alert("Sorry, there are no images matching your search query. Please try again.");
-    loadMoreBtn.style.display = 'none';
-    return;
-  }
+    if (images.hits.length === 0) {
+      Notiflix.Report.Failure('Oops!', 'Sorry, there are no images matching your search query. Please try again.', 'OK');
+      loadMoreBtn.style.display = 'none';
+      return;
+    }
 
-  currentPage += 1;
-  renderGallery(images.hits);
-  loadMoreBtn.style.display = 'block';
+    currentPage += 1;
+    renderGallery(images.hits);
+    loadMoreBtn.style.display = 'block';
 
-  if (currentPage > Math.ceil(images.totalHits / 40)) {
-    loadMoreBtn.style.display = 'none';
-    alert("We're sorry, but you've reached the end of search results.");
+    if (currentPage > Math.ceil(images.totalHits / 40)) {
+      loadMoreBtn.style.display = 'none';
+      Notiflix.Report.Info('End of Results', "We're sorry, but you've reached the end of search results.", 'OK');
+    }
+  } catch (error) {
+    Notiflix.Report.Failure('Oops!', 'There was an error fetching images. Please try again later.', 'OK');
+    console.error(error);
   }
 }
 
